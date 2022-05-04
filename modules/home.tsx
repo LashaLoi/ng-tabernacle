c13d7b93d2f2ac4d45fd10d05d6a7fc4af148961
Aliaksei Loi <alex.loi@pandadoc.com>
2022-05-04 11:48:27 +0300

    finish first page

--- modules/home.tsx
+++ modules/home.tsx
@@ -1,22 +1,38 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+import logo from '../public/logo.svg'
+
 export default function Home() {
   return (
     <div className="absolute top-0  w-full h-full flex justify-center">
       <div className="w-full mt-5 flex flex-col justify-center items-center">
-        <h1 className="main-title text-4xl mb-4">NEW GENERATION</h1>
-        <div className="h-[1px] w-28 bg-gradient-to-r from-[#c850c0] to-blue-500 mb-12"></div>
-        <div className="flex">
-          <button className="w-[130px] text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm text-center mx-2 mb-2">
-            Регистрация
-          </button>
-          <button className="w-[130px] text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-1 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mx-2 mb-2">
-            Программа
-          </button>
-          <button className="w-[130px] text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-1 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mx-2 mb-2">
-            Контакты
-          </button>
-          <button className="w-[130px] text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-1 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mx-2 mb-2">
-            Галерея
-          </button>
+        <div style={{ marginTop: -100 }}>
+          <Image src={logo.src} alt={logo.alt} height={200} width={300} />
+        </div>
+        <h1 className="main-title text-4xl mb-6">NEW GENERATION</h1>
+        <div className="h-[1px] w-[200px] bg-gradient-to-r from-[#c850c0] to-blue-500 mb-14"></div>
+        <div className="flex sm:flex-row flex-col">
+          <Link href="/registration">
+            <a className="sm:w-[130px] w-[200px] text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-1 focus:outline-none focus:ring-blue-300 font-medium text-sm sm:py-2 py-1 text-center mx-2 mb-4">
+              Регистрация
+            </a>
+          </Link>
+          <Link href="/program">
+            <a className="sm:w-[130px] w-[200px] text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-1 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium text-sm sm:py-2 py-1 text-center mx-2 mb-4">
+              Программа
+            </a>
+          </Link>
+          <Link href="/about">
+            <a className="sm:w-[130px] w-[200px] text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-1 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium text-sm sm:py-2 py-1 text-center mx-2 mb-4">
+              Контакты
+            </a>
+          </Link>
+          <Link href="/gallery">
+            <a className="sm:w-[130px] w-[200px] text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-1 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium text-sm sm:py-2 py-1 text-center mx-2 mb-4">
+              Галерея
+            </a>
+          </Link>
         </div>
       </div>
     </div>
