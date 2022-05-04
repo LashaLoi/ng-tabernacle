2a18beb8bc14a46914c004a7b643f43589bcf359
Aliaksei Loi <alex.loi@pandadoc.com>
2022-05-04 12:42:34 +0300

    add seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,28 @@
 import { Suspense } from 'react'
+import Head from 'next/head'
 
 import { Canvas } from '@react-three/fiber'
 import { Stars } from '../components/tree/star'
 import HomeModule from '../modules/home'
 
-const BG = () => (
-  <Canvas
-    dpr={[1, 2]}
-    camera={{ position: [0, 0, 10], fov: 5 }}
-    style={{ height: '100vh' }}
-  >
-    <Stars />
-  </Canvas>
-)
-
 export default function Home() {
   return (
-    <div className="h-screen w-screen">
-      <Suspense fallback={null}>
-        <BG />
-      </Suspense>
-      <HomeModule />
-    </div>
+    <>
+      <Head>
+        <title>Tabernacle</title>
+      </Head>
+      <div className="h-screen w-screen">
+        <Suspense fallback={null}>
+          <Canvas
+            dpr={[1, 2]}
+            camera={{ position: [0, 0, 10], fov: 5 }}
+            style={{ height: '100vh' }}
+          >
+            <Stars />
+          </Canvas>
+        </Suspense>
+        <HomeModule />
+      </div>
+    </>
   )
 }
